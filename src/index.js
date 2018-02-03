2af626834030116ca501947e3e59760ff29ba1b3
Jestoni Talan <jvtalan1@up.edu.ph>
2018-02-03 14:25:30 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>React App Running!</h1>
+    </div>
+  )
+}
+
+render (
+  <App/>,
+  document.getElementById('root')
+)
